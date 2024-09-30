77e06651931fd99890f670a5036f1b26d285a0fa
rachmadrf <rachmad17845@gmail.com>
2024-09-30 19:39:50 +0700

    update

---
+++ PemilihanBilangan21.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PemilihanBilangan21 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+        String hasil;
+
+        hasil = (angka % 2 == 0) ? "Bilangan genap" : "Bilangan ganjil";
+        
+        System.out.println(angka + " adalah " + hasil);
+
+    }
+}
